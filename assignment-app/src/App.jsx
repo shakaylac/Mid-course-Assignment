23684ae6d6d0f253b7f6f4baf953a8439b1d07c7
johncfrauen-lc101 <christian.frauenhoffer.cs@launchcode.org>
2024-11-12 22:36:36 -0700

    start app.jsx working

--- assignment-app/src/App.jsx
+++ assignment-app/src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
-        <Link className="navbar-brand ms-4 nav-link" to="/">
-          User Behavior Data
-        </Link>
+        {/* <p> tag is a placeholder, You'll need to change the tag/component type later*/}
+        <p className="navbar-brand ms-4 nav-link">User Behavior Data</p>
       </nav>
       <hr />
     </div>
