cd655b32cd6a2ed010eecf3dc56910e931041f5f
shakayla <kaycnite@gmail.com>
2025-01-28 15:19:50 -0600

    Added text info. h1 tag around key feature text. I put the bullet items in a ul tag and gave them all li tags. I gave the ul a classname of list-unstyled to get rid of the bullets. I added the Kaggle link in an a href tag.

--- assignment-app/src/Home.jsx
+++ assignment-app/src/Home.jsx
@@ -4,7 +4,25 @@ function Home(){
         <>
         <div className="container">
             <h1>User Behavior Dataset</h1>
-        
+                <p>This dataset provides a comprehensive analysis of mobile device usage patterns and user behavior classification. It contains 700 samples of user data, including metrics such as app usage time, screen-on time, battery drain, and data consumption. Each entry is categorized into one of five user behavior classes, ranging from light to extreme usage, allowing for insightful analysis and modeling.
+                 </p>
+            <h1>Key Features:</h1>
+                <ul className="list-unstyled">
+                    <li> - User ID: Unique identifier for each user. </li>
+                    <li> - Device Model: Model of the user's smartphone. </li>
+                    <li> - Operating System: The OS of the device (iOS or Android). </li>
+                    <li> - App Usage Time: Daily time spent on mobile applications, measured in minutes. </li>
+                    <li> - Screen On Time: Average hours per day the screen is active. </li>
+                    <li> - Battery Drain: Daily battery consumption in mAh. </li>
+                    <li> - Number of Apps Installed: Total apps available on the device. </li>
+                    <li> - Data Usage: Daily mobile data consumption in megabytes. </li>
+                    <li> - Age: Age of the user. </li>
+                    <li> - Gender: Gender of the user (Male or Female). </li>
+                    <li> - User Behavior Class: Classification of user behavior based on usage patterns (1 to 5).</li>
+                </ul>   
+                <a href="https://www.kaggle.com/datasets/valakhorasani/mobile-device-usage-and-user-behavior-dataset?resource=download
+">Sourced from this Kaggle Dataset
+                </a>
         </div>
         </>
     );
