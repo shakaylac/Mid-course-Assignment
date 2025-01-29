814b9ef884d0266335279905c11d5b58118c3c3b
shakaylac <146593316+shakaylac@users.noreply.github.com>
2025-01-29 10:17:33 -0600

    Update Search.jsx
    Added titles to columns.

--- assignment-app/src/Search.jsx
+++ assignment-app/src/Search.jsx
@@ -23,10 +23,10 @@ function Search() {
              <p> No Records To Display </p>
             <div className="container">
                 <div className="row">
-                  <div className="col"></div>
-                  <div className="col"></div>
-                  <div className="col"></div>
-                  <div className="col"></div>
+                  <div className="col">App Usage Time (min/day) </div>
+                  <div className="col">Screen on time (hours/day)</div>
+                  <div className="col">Number of Apps Installed</div>
+                  <div className="col">Age</div>
                 </div>
             </div>
             </div>
