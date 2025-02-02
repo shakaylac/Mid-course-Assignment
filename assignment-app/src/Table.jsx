7cf5a3d2b5deac0337b4ca51bfc83343d771ef35
shakayla <kaycnite@gmail.com>
2025-02-01 20:22:03 -0600

    Added header to table.

---
+++ assignment-app/src/Table.jsx
@@ -0,0 +1,28 @@
+
+
+function Table() {
+    return(
+        <table>
+            <thead>
+            <tr className="fw-bold">
+                <th scope="col">User ID</th>
+                <th scope="col">Device Model</th>
+                <th scope="col">Operating System</th>
+                <th scope="col">App Usage Time (min/day)</th>
+                <th scope="col">Screen On Time (hours/day)</th>
+                <th scope="col">Battery Drain (mAh/day)</th>
+                <th scope="col">Number of Apps Installed</th>
+                <th scope="col">Data Usage (MB/day)</th>
+                <th scope="col">Age</th>
+                <th scope="col">Gender</th>
+                <th scope="col">User Behavior Class</th>
+            </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
