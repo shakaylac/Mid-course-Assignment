57d0f674dbc415d7cfcb79a2fac13e612a2784bf
shakayla <kaycnite@gmail.com>
2025-02-01 20:50:54 -0600

    Added styling to search bar and search button.

--- assignment-app/src/Search.jsx
+++ assignment-app/src/Search.jsx
@@ -12,19 +12,21 @@ function Search() {
         <div className="container">
             <DropdownButton />
 
-            <input type="text" placeholder="Search by Keyword"></input>
-            <button>Search</button>
-             <p> No Records To Display </p>
-            <div className="container">
-                <div className="row">
-                  <div className="col border">App Usage Time (min/day) </div>
-                  <div className="col border">Screen on time (hours/day)</div>
-                  <div className="col border">Number of Apps Installed</div>
-                  <div className="col border">Age</div>
-                </div>
+            <div className="d-flex flex-column w-25">
+                <input type="text" placeholder="Search by Keyword" className="mb-2 mt-2 rounded border-secondary-subtle"></input>
+                <button className="rounded border-0">Search</button>
             </div>
+                <p> No Records To Display </p>
+                <div className="container">
+                    <div className="row">
+                    <div className="col border">App Usage Time (min/day) </div>
+                    <div className="col border">Screen on time (hours/day)</div>
+                    <div className="col border">Number of Apps Installed</div>
+                    <div className="col border">Age</div>
+                    </div>
+                </div>
 
-            <Table />
+                <Table />
             </div>
      
         </>
