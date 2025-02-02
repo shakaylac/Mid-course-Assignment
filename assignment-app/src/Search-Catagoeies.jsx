773e797742b068c221f62bb126ffc388aa09c907
shakayla <kaycnite@gmail.com>
2025-02-01 19:14:54 -0600

    Deleted file. It wasn't needed.

--- assignment-app/src/Search-Catagoeies.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-import {useEffect, useState} from "react";
-import Search from "./search";
-
-export default function Search-Catorgories() {
-  const use data = [
-
-   ]
-}
--- assignment-app/src/Search.jsx
+++ assignment-app/src/Search.jsx
@@ -23,7 +23,7 @@ function Search() {
                 </div>
             </div>
             </div>
-            </div>      
+     
         </>
     );
 };
