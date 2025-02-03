0e36f9ffdf4a9c783052d5002776d65576a65893
shakayla <kaycnite@gmail.com>
2025-02-02 21:00:54 -0600

    Changed seni-colon to hyphens in avg and med component.

--- assignment-app/src/AverageandMedian.jsx
+++ assignment-app/src/AverageandMedian.jsx
@@ -8,29 +8,29 @@ function AverageandMedian() {
             <div className="border w-25">
                 <p><strong>Average Usage Time (min/day)</strong></p>
 
-                <p>Average: </p>
-                <p>Median: </p>
+                <p>Average- </p>
+                <p>Median- </p>
             </div>
 
             <div className="border w-25">
                 <p><strong>Screen On Time (hours/day)</strong></p>
 
-                <p>Average: </p>
-                <p>Median: </p>
+                <p>Average- </p>
+                <p>Median- </p>
             </div>
 
             <div className="border w-25">
                 <p><strong>Number of Apps Installed</strong></p>
 
-                <p>Average: </p>
-                <p>Median: </p>
+                <p>Average- </p>
+                <p>Median- </p>
             </div>
 
             <div className="border w-25">
                 <p><strong>Age</strong></p>
 
-                <p>Average: </p>
-                <p>Median: </p>
+                <p>Average- </p>
+                <p>Median- </p>
             </div>
 
         </div>
