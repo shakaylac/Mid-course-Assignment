deebd02205f54337885afee02982d2eeac1d00d9
shakayla <kaycnite@gmail.com>
2025-02-04 19:34:55 -0600

    Changed value to data-value, added e.target.innerText so I can change the text in in the dropdown button to mt the selected option. I took away the onclick from the ul and put it on the individual li.

--- assignment-app/src/DropdownButton.jsx
+++ assignment-app/src/DropdownButton.jsx
@@ -6,8 +6,8 @@ function DropdownButton({ onFilterChange}) {
     const [selectedOption, setSelectedOption] = useState('Model');
  
     const handleChange = (e) => {
-       const value =e.target.getAttribute("value");
-       setSelectedOption(value);
+       const value =e.target.getAttribute("data-value");
+       setSelectedOption(e.target.innerText);
        onFilterChange(value);
 
 
@@ -19,11 +19,11 @@ function DropdownButton({ onFilterChange}) {
         <p>Select data point to filter search by</p>
         <div className="input-group">
           <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
-            <ul className="dropdown-menu" value={selectedOption} onChange={handleChange}>
-                <li className="dropdown-item" value="model">Model</li>
-                <li className="dropdown-item" value="gender">Gender</li>
-                <li className="dropdown-item" value="operating-system">Operating System</li>
-                <li className="dropdown-item" value="behavior-class">Behavior Class</li>
+            <ul className="dropdown-menu">
+                <li className="dropdown-item" data-value="model" onClick={handleChange}>Model</li>
+                <li className="dropdown-item" data-value="gender" onClick={handleChange}>Gender</li>
+                <li className="dropdown-item" data-value="operating-system" onClick={handleChange}>Operating System</li>
+                <li className="dropdown-item" data-value="behavior-class" onClick={handleChange}>Behavior Class</li>
             </ul>
           </div>
         </div>
