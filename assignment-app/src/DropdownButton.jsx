ec2a1f12025d3fb5936c7823af0e482992fb5d79
shakayla <kaycnite@gmail.com>
2025-02-04 19:49:19 -0600

    Added the selectedOpitonin between the greater than and lesser than signs. I thought it wasn't working and tried adding things but they weren't neededso I deleted them. Dropdown works as intended now.

--- assignment-app/src/DropdownButton.jsx
+++ assignment-app/src/DropdownButton.jsx
@@ -3,13 +3,12 @@ import { useState } from "react";
 function DropdownButton({ onFilterChange}) {
 
 
-    const [selectedOption, setSelectedOption] = useState('Model');
+    const [selectedOption, setSelectedOption] = useState('');
  
     const handleChange = (e) => {
-       const value =e.target.getAttribute("data-value");
+       const value = e.target.getAttribute("data-value");
        setSelectedOption(e.target.innerText);
        onFilterChange(value);
-
 
     }
 
@@ -18,7 +17,7 @@ function DropdownButton({ onFilterChange}) {
         <div className="d-flex flex-column">
         <p>Select data point to filter search by</p>
         <div className="input-group">
-          <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+          <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">{selectedOption}</button>
             <ul className="dropdown-menu">
                 <li className="dropdown-item" data-value="model" onClick={handleChange}>Model</li>
                 <li className="dropdown-item" data-value="gender" onClick={handleChange}>Gender</li>
