ffdf07a68fea355571da889fcf15c88b85c83ffb
shakayla <kaycnite@gmail.com>
2025-03-27 07:19:31 -0500

    Deleted cors from the server.js and added all the localhost that the page can have. I did this because the current url that's in the server.js doesn't always match. Now there should be no problem with the page acessing the api.

--- api/server.js
+++ api/server.js
@@ -3,13 +3,20 @@ const app = express();
 const port = 3000;
 const apiRoutes = require("./routes");
 const qs = require("qs");
-const cors = require('cors');
+const allowedOrigins = ['http://localhost:5175','http://localhost:5174','http://localhost:5173'];
 
 // Root route
 
-app.use(cors({
-  origin: 'http://localhost:5175',
-}));
+app.use((req, res, next) =>{
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Aunthorization');
+  next();
+});
+
 
 app.get('http://localhost:3000/api/data/search', (req, res) => {
   res.json({message: 'Data fetched successfully'});
